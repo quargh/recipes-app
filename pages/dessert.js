3f7aa4157787dde44e49ec0f2b8088e59f70ae1d
Stefan Helms <mail@estefan.de>
2022-08-02 15:03:30 +0200

    "Dritter Commit via Webstorm interface"

--- pages/dessert.js
+++ pages/dessert.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Dessert() {
       return (
@@ -8,6 +9,13 @@ export default function Dessert() {
                 </Head>
                 <h1>Dessert</h1>
                 <p>List of dessert recipes</p>
+                <Image
+                    src="https://unsplash.com/photos/surQ2mkZNxw/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mzd8fGRlc3NlcnR8ZW58MHwwfHx8MTY1OTMzNzk1NQ&force=true&w=2400"
+                    alt="Schöne Fische"
+                    layout="responsive"
+                    width={2400}
+                    height={1599}
+                />
           </>
       );
 }
