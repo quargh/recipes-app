4032c312ea717f8f557a903701381a9964d25626
Stefan Helms <mail@estefan.de>
2022-08-02 14:25:35 +0200

    "Erster Commit via Webstorm interface"

---
+++ pages/dessert.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Dessert() {
+      return (
+          <>
+                <Head>
+                      <title>Dessert Section</title>
+                </Head>
+                <h1>Dessert</h1>
+                <p>List of dessert recipes</p>
+          </>
+      );
+}
---
+++ pages/veggie.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Veggie() {
+      return (
+          <>
+                <Head>
+                      <title>Veggie Section</title>
+                </Head>
+                <h1>Veggie</h1>
+                <p>List of veggie recipes</p>
+          </>
+      );
+}
